3079cccdf8c9ec64ca0d661477035067720d7c0e
Abhishek Kumar <jhabhishek15@gmail.com>
2022-12-21 23:20:44 +0530

    Add project files.

---
+++ TestProject1/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    internal class TestBase
+    {
+        //here you can add steps to read data from json
+        JsonConvert.DeserializeObject<>(System.IO.File.ReadAllText());
+
+    }
+}
---
+++ TestProject1/TestSetup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    internal class TestSetup
+    {
+
+        public void RunBeforeAnyTest()
+        {
+            if(TestContext.Parameters.Count == 0)
+            {
+                throw new Exception("Run Setting file is not selected");
+            }
+
+            
+
+        }
+    }
+}
