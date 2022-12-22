12d8d8645406fda1c736faf8ec0ccaa826578d57
Abhishek Kumar <jhabhishek15@gmail.com>
2022-12-22 23:13:18 +0530

    Added line for reading data from json, created few folders like data and models.

---
+++ TestProject1/Model/WatchesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1.Model
+{
+    public class WatchesModel
+    {
+
+            public string WatchBrand { get; set; }
+            public string WatchType { get; set; }
+            public string WatchSTrap { get; set; }
+        
+
+    }
+}
--- TestProject1/TestBase.cs
+++ TestProject1/TestBase.cs
@@ -1,16 +1,44 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V106.Browser;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestProject1.Model;
 
 namespace TestProject1
 {
     internal class TestBase
     {
-        //here you can add steps to read data from json
-        JsonConvert.DeserializeObject<>(System.IO.File.ReadAllText());
+        internal IWebDriver driver;
+        internal dynamic  Browser;
+        public static List<WatchesModel> Watch;
+
+        //defing the scope of "Watch" at class level which can be use project wise after inheriting
+        public void SetUp()
+        {
+            var CurrentDirectory = AppContext.BaseDirectory;
+            //here you can add steps to read data from json
+            Watch = JsonConvert.DeserializeObject<List<WatchesModel>>
+            (System.IO.File.ReadAllText(Path.Combine(CurrentDirectory,"data","","Watches.json")));
+
+            //You can also read multiple json from testbase. 
+
+            
+            
+
+        }
+
+        //Pending todo -- to read data from runsetting files
+        public void TestInitialization(TestContext testContext)
+        {var  Browser = testContext.["Browser"].ToString(); }
+
+
+        
 
     }
+    
 }
